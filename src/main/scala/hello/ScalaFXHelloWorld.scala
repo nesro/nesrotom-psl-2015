3c5ea71f58be5542d58ef36fe922384a16428c17
nesro <nesro@nesro.cz>
2015-06-10 12:40:34 +0200

    mandelbrot

--- src/main/scala/hello/ScalaFXHelloWorld.scala
+++ src/main/scala/hello/ScalaFXHelloWorld.scala
@@ -2,6 +2,7 @@
  * Copyright (c) 2015, Tomas Nesrovnal (nesrotom@fit.cvut.cz)
  * This is a semestral work for MI-PSL 2015 (programming in scala) lecture
  * on FIT CTU. See: https://edux.fit.cvut.cz/courses/MI-PSL/
+ * docs: http://docs.scalafx.googlecode.com/hg/scalafx-8.0/scaladoc/index.html
  *
  * Copyright (c) 2011-2015, ScalaFX Project
  * All rights reserved.
@@ -36,84 +37,349 @@ import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.VBox
+import scalafx.scene.paint.Color
 import scalafx.scene.paint.Color._
-import scalafx.scene.paint.{LinearGradient, Stops}
+//import scalafx.scene.paint.{LinearGradient, Stops}
 import scalafx.scene.text.Text
 
 import scalafx.scene.shape.Rectangle
 import scalafx.Includes._ // when
 
-import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.image.{Image, ImageView, PixelWriter, WritableImage}
 
-import scalafx.scene.control.Button
+import scalafx.scene.control.{Button, ProgressBar}
 
 import scalafx.event.ActionEvent
+import scalafx.scene.input.MouseEvent
 
+import scala.collection._
+import scala.collection.parallel.mutable.ParArray
+
+/*
+import scala.math.BigDecimal
+*/
 
 object ScalaFXHelloWorld extends JFXApp {
 	stage = new PrimaryStage {
 	title = "nesrotom MI-PSL 2015 semestral work"
 	scene = new Scene {
 
-	val rect = new Rectangle {
-		x = 25
-		y = 40
-		width = 100
-		height = 100
-		fill <== when (hover) choose Green otherwise Red
+	var xsize = 500.0
+	var ysize = 500.0
+	var ymin = -5.0 
+	var ymax = 5.0 
+	var xmin = -5.0 
+	var xmax = 5.0 
+	var power = 15.0
+	var threshold = 15
+	var xclick = 0.0 
+	var yclick = 0.0 
+	var zoomed = 0
+	var xzoom = 0.0 
+	var yzoom = 0.0 
+	var zoom = 0.0 
+	var xstep = 0.0 
+	var ystep = 0.0 
+
+	//val pixels = new Array[Int]((xsize * ysize).toInt)
+	val pixels = new ParArray[Int]((xsize * ysize).toInt)
+
+	def reset() {
+		ymin = -5.0
+		ymax = 5.0
+		xmin = -5.0
+		xmax = 5.0
+		power = 15.0
+		threshold = 15
+		xclick = 0.0
+		yclick = 0.0
+		zoomed = 0
+		xzoom = 0.0
+		yzoom = 0.0
+		zoom = 0.0
+		xstep = 0.0
+		ystep = 0.0
+		draw()
 	}
 
-	val rect2 = new Rectangle {
-		x = 50
-		y = 100
-		width = 10
-		height = 10
-		fill = Black
-	}
-
-	val btn = new Button("click me") {
-	}
-
-	btn.onAction = {ae: ActionEvent => rect2.fill = White }
-
-	fill = Grey
-	content = new HBox {
-	padding = Insets(20)
-	children = Seq(
-		new Text {
-			text = "MI-PSL "
-			style = "-fx-font-size: 10pt"
-			fill = new LinearGradient(
-			endX = 0,
-			stops = Stops(PaleGreen, SeaGreen))
-		},
-		new Text {
-			text = "is the best :D"
-			style = "-fx-font-size: 10pt"
-			fill = new LinearGradient(
-				endX = 0,
-				stops = Stops(Cyan, DodgerBlue)
-			)
-			effect = new DropShadow {
-				color = DodgerBlue
-				radius = 25
-				spread = 0.25
-			}
-		},
-		rect,
-		rect2,
-		btn,
-		new ImageView {
-			//image = new Image(this.getClass.getResourceAsStream("/scalafx/ensemble/images/icon-48x48.png"))
-			image = new Image("http://www.scala-lang.org/resources/img/scala-logo.png")
-			fitHeight = 50
-			fitWidth = 150
-			preserveRatio = true
-			smooth = true
+	def scale(x: Int, min: Int, max: Int, a: Int, b: Int): Int = {
+		if (max == min) {
+			0
+		} else {
+			(((b-a)*(x-min))/(max - min)) + a
 		}
+	}
+
+	def mb(ix: Double, iy: Double, threshold: Int): Int = {
+		var i = threshold
+		var x = 0.0 
+		var y = 0.0 
+		while (x * x + y * y < 2 && 0 < i) {
+			val yy = 2 * x * y + iy 
+			val xx = x * x - y * y + ix 
+			y = yy
+			x = xx
+			i -= 1
+		}
+		threshold - i
+	}
+
+
+	val iv = new ImageView {
+		//image = new Image(this.getClass.getResourceAsStream("/scalafx/ensemble/images/icon-48x48.png"))
+		image = new Image("http://www.scala-lang.org/resources/img/scala-logo.png")
+		fitHeight = ysize.toInt 
+		fitWidth = xsize.toInt
+		//preserveRatio = true
+		//smooth = true
+	}
+	val wi = new WritableImage(xsize.toInt, ysize.toInt)
+	val pw = wi.pixelWrit
+
+	var b = new Button("reset")
+	b.onAction = (_:ActionEvent)=> {
+		reset()
+	}
+
+	def draw() {
+		var imin = 99999
+		var imax = -99999
+		var cx = (xmax - xmin) / xsize
+		var cy = (ymax - ymin) / ysize
+		var progress = 0
+		//for (ix <- 0 to xsize.toInt - 1; iy <- 0 to ysize.toInt - 1) {
+		for (ix <- (0 until xsize.toInt).par) {
+			for (iy <- 0 until ysize.toInt) {
+				val c = mb(xmin + cx*ix, ymin + cy*iy, power.toInt)
+				pixels(ix + iy * ysize.toInt) = c
+				if (c < imin) {
+					imin = c
+				}
+				if (c > imax) {
+					imax = c
+				}
+			}
+			if ((ix % (xsize/10)) == 0) {
+				progress = progress + 10
+				println(s"computed: $progress %")
+			}
+		}
+		println(s"imax=$imax")
+		println(s"imin=$imin")
+
+		for (ix <- 0 to xsize.toInt - 1; iy <- 0 to ysize.toInt - 1) {
+			val cr = scale(pixels(ix + xsize.toInt * iy), imin, imax, 0, (255*255*255))
+			pw.setColor(ix, iy, Color.web(f"$cr%06x"))
+		}
+		iv.image = wi
+	}
+
+	iv.onMouseClicked = (e: MouseEvent) => {
+		yclick = ymin + (ymax - ymin) * (( e.y.toDouble) / ysize)
+		xclick = xmin + (xmax - xmin) * (( e.x.toDouble) / xsize)
+		yzoom = (ymax - ymin) / 4.0
+		xzoom = (xmax - xmin) / 4.0
+		ymin = yclick - yzoom
+		ymax = yclick + yzoom
+		xmin = xclick - xzoom
+		xmax = xclick + xzoom
+		ystep = (ymax - ymin) / ysize
+		xstep = (xmax - xmin) / xsize
+		power = power * 1.2
+		threshold = (threshold.toDouble * 1.2).toInt
+		zoomed = zoomed + 1
+		
+		var x = xmin
+		var y = ymin
+		val range = 0 to ((xsize.toInt * ysize.toInt))
+		println(s"xzoom: $xzoom")
+		println(s"yzoom: $yzoom")
+		println(s"xclick: $xclick")
+		println(s"yclick: $yclick")
+		println(s"xmin: $xmin")
+		println(s"xmax: $xmax")
+		println(s"ymin: $ymin")
+		println(s"ymax: $ymax")
+		println(s"xstep: $xstep")
+		println(s"ystep: $ystep")
+
+		draw()
+	}
+
+	draw()
+
+	fill = Grey 
+	content = new VBox {
+	margin = Insets(25)
+	padding = Insets(25)
+	children = Seq(
+		iv,
+		b
 	)
 	}
 	}
 	}
 }
+/*
+object ScalaFXHelloWorld extends JFXApp {
+	stage = new PrimaryStage {
+	title = "nesrotom MI-PSL 2015 semestral work"
+	scene = new Scene {
+
+	var xsize = 500.0
+	var ysize = 500.0
+	var ymin = -5.0 : BigDecimal
+	var ymax = 5.0 : BigDecimal
+	var xmin = -5.0 : BigDecimal
+	var xmax = 5.0 : BigDecimal
+	var power = 15.0
+	var threshold = 15
+	var xclick = 0.0 : BigDecimal
+	var yclick = 0.0 : BigDecimal
+	var zoomed = 0
+	var xzoom = 0.0 : BigDecimal
+	var yzoom = 0.0 : BigDecimal
+	var zoom = 0.0 : BigDecimal
+	var xstep = 0.0 : BigDecimal
+	var ystep = 0.0 : BigDecimal
+
+	//val pixels = new Array[Int]((xsize * ysize).toInt)
+	val pixels = new ParArray[Int]((xsize * ysize).toInt)
+
+	def reset() {
+		ymin = -5.0
+		ymax = 5.0
+		xmin = -5.0
+		xmax = 5.0
+		power = 15.0
+		threshold = 15
+		xclick = 0.0
+		yclick = 0.0
+		zoomed = 0
+		xzoom = 0.0
+		yzoom = 0.0
+		zoom = 0.0
+		xstep = 0.0
+		ystep = 0.0
+		draw()
+	}
+
+	def scale(x: Int, min: Int, max: Int, a: Int, b: Int): Int = {
+		if (max == min) {
+			0
+		} else {
+			(((b-a)*(x-min))/(max - min)) + a
+		}
+	}
+
+	def mb(ix: BigDecimal, iy: BigDecimal, threshold: Int): Int = {
+		var i = threshold
+		var x = 0.0 : BigDecimal
+		var y = 0.0 : BigDecimal
+		while (x * x + y * y < 2 && 0 < i) {
+			val yy = 2 * x * y + iy : BigDecimal
+			val xx = x * x - y * y + ix : BigDecimal
+			y = yy
+			x = xx
+			i -= 1
+		}
+		threshold - i
+	}
+
+
+	val iv = new ImageView {
+		//image = new Image(this.getClass.getResourceAsStream("/scalafx/ensemble/images/icon-48x48.png"))
+		image = new Image("http://www.scala-lang.org/resources/img/scala-logo.png")
+		fitHeight = ysize.toInt 
+		fitWidth = xsize.toInt
+		//preserveRatio = true
+		//smooth = true
+	}
+	val wi = new WritableImage(xsize.toInt, ysize.toInt)
+	val pw = wi.pixelWrit
+
+	var b = new Button("reset")
+	b.onAction = (_:ActionEvent)=> {
+		reset()
+	}
+
+	def draw() {
+		var imin = 99999
+		var imax = -99999
+		var cx = (xmax - xmin) / xsize
+		var cy = (ymax - ymin) / ysize
+		var progress = 0
+		//for (ix <- 0 to xsize.toInt - 1; iy <- 0 to ysize.toInt - 1) {
+		for (ix <- (0 until xsize.toInt).par) {
+			for (iy <- 0 until ysize.toInt) {
+				val c = mb(xmin + cx*ix, ymin + cy*iy, power.toInt)
+				pixels(ix + iy * ysize.toInt) = c
+				if (c < imin) {
+					imin = c
+				}
+				if (c > imax) {
+					imax = c
+				}
+			}
+			if ((ix % (xsize/10)) == 0) {
+				progress = progress + 10
+				println(s"computed: $progress %")
+			}
+		}
+		println(s"imax=$imax")
+		println(s"imin=$imin")
+
+		for (ix <- 0 to xsize.toInt - 1; iy <- 0 to ysize.toInt - 1) {
+			val cr = scale(pixels(ix + xsize.toInt * iy), imin, imax, 0, (255*255*255))
+			pw.setColor(ix, iy, Color.web(f"$cr%06x"))
+		}
+		iv.image = wi
+	}
+
+	iv.onMouseClicked = (e: MouseEvent) => {
+		yclick = ymin + (ymax - ymin) * (( e.y.toDouble) / ysize)
+		xclick = xmin + (xmax - xmin) * (( e.x.toDouble) / xsize)
+		yzoom = (ymax - ymin) / 4.0
+		xzoom = (xmax - xmin) / 4.0
+		ymin = yclick - yzoom
+		ymax = yclick + yzoom
+		xmin = xclick - xzoom
+		xmax = xclick + xzoom
+		ystep = (ymax - ymin) / ysize
+		xstep = (xmax - xmin) / xsize
+		power = power * 1.2
+		threshold = (threshold.toDouble * 1.2).toInt
+		zoomed = zoomed + 1
+		
+		var x = xmin
+		var y = ymin
+		val range = 0 to ((xsize.toInt * ysize.toInt))
+		println(s"xzoom: $xzoom")
+		println(s"yzoom: $yzoom")
+		println(s"xclick: $xclick")
+		println(s"yclick: $yclick")
+		println(s"xmin: $xmin")
+		println(s"xmax: $xmax")
+		println(s"ymin: $ymin")
+		println(s"ymax: $ymax")
+		println(s"xstep: $xstep")
+		println(s"ystep: $ystep")
+
+		draw()
+	}
+
+	draw()
+
+	fill = Grey 
+	content = new VBox {
+	margin = Insets(25)
+	padding = Insets(25)
+	children = Seq(
+		iv,
+		b
+	)
+	}
+	}
+	}
+}*/
